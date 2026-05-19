06a769beda2138c83fd1115a1af2c91536bf647a
zeshannkhan7 <124658200+zeshannkhan7@users.noreply.github.com>
2026-05-19 20:24:03 +0500

    Delete firebase-config (1).js

--- firebase-config (1).js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBznpvsRiTsEgkhTfrRUA_QSYERgUx8C1U",
-  authDomain: "alkhidmat-islamabad.firebaseapp.com",
-  projectId: "alkhidmat-islamabad",
-  storageBucket: "alkhidmat-islamabad.firebasestorage.app",
-  messagingSenderId: "547717382330",
-  appId: "1:547717382330:web:cc4a87326c747c86f4c283"
-};
